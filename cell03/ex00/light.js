5614853b01728099bfdda582cef6145903872469
Néstor Ariel Tamayo Franco <ntamayo-@student.42malaga.com>
2022-10-12 14:47:07 +0200

    Random colorfulness

---
+++ cell03/ex00/light.js
@@ -0,0 +1,8 @@
+function getColour() {
+	const r = Math.floor(Math.random() * 244) + 12;
+	const g = Math.floor(Math.random() * 244) + 12;
+	const b = Math.floor(Math.random() * 244) + 12;
+
+	document.body.style.background = `rgb(${r}, ${g}, ${b})`;
+	console.log("Color displayed: ", r, g, b);
+}
